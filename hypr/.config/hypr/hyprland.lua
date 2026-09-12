33dd724c356233c355646f9b7c08f8d395e822ca
dsaezgonzalez <diego.saezg11@gmail.com>
2026-09-12 13:07:46 +0100

    Fix laptop display scaling

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -5,6 +5,13 @@
 ---- MONITORS ----
 ------------------
 hl.monitor({
+    output   = "eDP-1",
+    mode     = "preferred",
+    position = "auto",
+    scale    = 1,
+})
+
+hl.monitor({
     output   = "DP-1",
     mode     = "2560x1440@180",
     position = "0x0",
